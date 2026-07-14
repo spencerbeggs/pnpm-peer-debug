8c2a46d846239c7409fc1affaad688423b06c422
C. Spencer Beggs <spencer@savvyweb.systems>
2026-07-14 02:55:10 -0400

    chore: stash
    Signed-off-by: C. Spencer Beggs <spencer@beggs.codes>

--- packages/example/src/index.ts
+++ packages/example/src/index.ts
@@ -1,10 +1,19 @@
 import { Schema } from "effect";
+import type { Effect } from "effect/Effect";
+import type { Option } from "effect/Option";
+import type { Check, Objects } from "effect/SchemaAST";
 /**
  * An animal
  * @public
  */
 export class Animal {
+	/**
+	 * The name of the animal
+	 */
 	name: string;
+	/**
+	 * The date the animal was created
+	 */
 	createdAt: Date;
 
 	constructor(name: string, createdAt: Date) {
@@ -15,17 +24,52 @@ export class Animal {
 /**
  * A cat
  * @public
- */ export class Cat extends Animal {
+ */
+export class Cat extends Animal {
 	meow(): string {
 		return `${this.name} says: Meow!`;
 	}
 }
 
 /**
+ * A base class
+ * @public
+ */
+export class Base {
+	/**
+	 * Some documentation that we want to inherit
+	 */
+	public member(): void {}
+}
+
+/**
+ * An interface representing a child
+ * @public
+ */
+export interface IChild {
+	/**
+	 * Some documentation that we want to inherit
+	 */
+	value: string;
+}
+
+/**
+ * A child class that extends the base class and implements the IChild interface
+ * @public
+ */
+export class Child extends Base implements IChild {
+	/** {@inheritDoc Base.member} */
+	public member(): void {}
+
+	/** {@inheritDoc IChild.value} */
+	public value: string = "example";
+}
+
+/**
  * A person
  * @public
  */
-export class EffectClass extends Schema.Class<EffectClass>("EffectClass")(
+export class Person extends Schema.Class<Person>("Person")(
 	Schema.Struct({
 		/**
 		 * The name of the person
@@ -33,7 +77,7 @@ export class EffectClass extends Schema.Class<EffectClass>("EffectClass")(
 		name: Schema.String,
 		/**
 		 * The date the person was created
-		 * */
+		 */
 		createdAt: Schema.Date,
 	}),
 ) {
